61b1ceb2d0b86f0b06c4ebd0a930d4725bc3de20
Jiankai Zeng <janpoem@163.com>
2020-12-09 23:18:10 +0800

    refactor: 恢复 trim.js 代码，删除多余的文件

--- src/trim.js
+++ src/trim.js
@@ -1,14 +1,26 @@
+import isString from 'lodash/isString';
+import { toSafeString } from '../trim';
+
+const isArray = Array.isArray;
+
+const TrimBoth = 0;
+const TrimLeft = 1;
+const TrimRight = -1;
+
+const PlusCharList = true;
+const ReplaceCharList = false;
+
 /**
  * default white space char list
  *
- *    @see http://php.net/manual/en/function.trim.php
+ * @see http://php.net/manual/en/function.trim.php
  *
- *    " " (ASCII 32 (0x20)), an ordinary space.
- *    "\t" (ASCII 9 (0x09)), a tab.
- *    "\n" (ASCII 10 (0x0A)), a new line (line feed).
- *    "\r" (ASCII 13 (0x0D)), a carriage return.
- *    "\0" (ASCII 0 (0x00)), the NUL-byte.
- *    "\x0B" (ASCII 11 (0x0B)), a vertical tab.
+ * " " (ASCII 32 (0x20)), an ordinary space.
+ * "\t" (ASCII 9 (0x09)), a tab.
+ * "\n" (ASCII 10 (0x0A)), a new line (line feed).
+ * "\r" (ASCII 13 (0x0D)), a carriage return.
+ * "\0" (ASCII 0 (0x00)), the NUL-byte.
+ * "\x0B" (ASCII 11 (0x0B)), a vertical tab.
  */
 const DefaultWhitespace = [
   ' ',
@@ -37,3 +49,116 @@ const DefaultWhitespace = [
   '\u3000'
 ].join('');
 
+/**
+ * 转换字符列表，只将数组和字符串输出，其他返回空白字符串
+ *
+ * @param {string|string[]} charList
+ * @return {string}
+ */
+function convertCharList(charList: string | string[]) {
+  if (isArray(charList)) {
+    charList = charList.join('');
+  }
+  if (isString(charList) && charList !== '') {
+    return toSafeString(charList);
+  }
+  return '';
+}
+
+
+/**
+ * PHP的字符串截取函数
+ *
+ * @see http://locutus.io/php/strings/trim/
+ * @see http://php.net/manual/en/function.trim.php
+ *
+ * @param {string} str 要截取的字符串
+ * @param {string|null|undefined|array} charList 要截取的字符列表
+ * @param {boolean} isPlus 字符列表是否为追加模式，默认为True，即以追加模式进行截取。默认PHP的模式，为指定了charlist后，就会替换掉默认的charlist
+ * @param {number} mode 截取模式，是两边截取，还是只截取左边 或 右边。
+ * @returns {string}
+ */
+export function trim(
+  str: string,
+  charList: string | string[] = null,
+  isPlus: boolean             = PlusCharList,
+  mode: number                = TrimBoth
+) {
+  var whitespace = DefaultWhitespace;
+  var l = 0;
+  var i = 0;
+  // @todo how about 0xD800 ~ 0xDBFF ?
+  str = toSafeString(str);
+  charList = convertCharList(charList);
+
+  if (charList !== '') {
+    charList = charList.replace(/([[\]().?/*{}+$^:])/g, '$1');
+    // 这里进行了修改，isPlus = true时为追加模式
+    if (!!isPlus) {
+      whitespace += charList;
+    } else {
+      whitespace = charList;
+    }
+  }
+  // 0 or > 0
+  l = str.length;
+  if (mode >= TrimBoth && l > 0) {
+    for (i = 0; i < l; i++) {
+      if (whitespace.indexOf(str.charAt(i)) === -1) {
+        str = str.substring(i);
+        break;
+      }
+    }
+  }
+  l = str.length;
+  if (mode <= TrimBoth && l > 0) {
+    for (i = l - 1; i >= 0; i--) {
+      if (whitespace.indexOf(str.charAt(i)) === -1) {
+        str = str.substring(0, i + 1);
+        break;
+      }
+    }
+  }
+
+  if (mode >= TrimBoth) {
+    return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
+  }
+  return whitespace.indexOf(str.charAt(str.length - 1)) === -1 ? str : '';
+}
+
+trim.Replace = ReplaceCharList;
+trim.Plus = PlusCharList;
+
+/**
+ * 左截取
+ *
+ * @see http://php.net/manual/en/function.rtrim.php
+ * @param {string} str 要截取的字符串
+ * @param {string|null|undefined|array} charList 要截取的字符列表
+ * @param {boolean} isPlus 字符列表是否为追加模式，默认为True，即以追加模式进行截取。默认PHP的模式，为指定了 charList 后，就会替换掉默认的charlist
+ * @return {string}
+ */
+export function ltrim(
+  str: string,
+  charList: string | string[] = null,
+  isPlus: boolean             = PlusCharList
+) {
+  return trim(str, charList, isPlus, TrimLeft);
+}
+
+/**
+ * 右截取
+ *
+ * @see http://php.net/manual/en/function.ltrim.php
+ * @param {string} str 要截取的字符串
+ * @param {string|null|undefined|array} charList 要截取的字符列表
+ * @param {boolean} isPlus 字符列表是否为追加模式，默认为True，即以追加模式进行截取。默认PHP的模式，为指定了 charList 后，就会替换掉默认的charlist
+ * @return {string}
+ */
+export function rtrim(
+  str: string,
+  charList: string | string[] = null,
+  isPlus: boolean             = PlusCharList
+) {
+  return trim(str, charList, isPlus, TrimRight);
+}
--- tests/trim.test.js
+++ tests/trim.test.js
@@ -1,89 +1,47 @@
-const {trim, ltrim, rtrim, toSafeString, isString, isEmptyString, isEmptyStringOrWhitespace} = require('../trim');
+import { trim, ltrim, rtrim } from '../src/trim';
 
 describe('trim test', () => {
 
-	it('trim basic test', () => {
-		expect(trim(' 你好 ')).toBe('你好');
-		expect(trim(' aaaa')).toBe('aaaa');
-		expect(trim('bbb ')).toBe('bbb');
-		expect(trim('   bbb b b   ')).toBe('bbb b b');
+  it('trim basic test', () => {
+    expect(trim(' 你好 ')).toBe('你好');
+    expect(trim(' aaaa')).toBe('aaaa');
+    expect(trim('bbb ')).toBe('bbb');
+    expect(trim('   bbb b b   ')).toBe('bbb b b');
 
-		expect(trim("bbb b b\t\t")).toBe('bbb b b');
-		expect(trim("bbb b b\t\n\r")).toBe('bbb b b');
-	});
+    expect(trim('bbb b b\t\t')).toBe('bbb b b');
+    expect(trim('bbb b b\t\n\r')).toBe('bbb b b');
+  });
 
-	it('trim custom charlist test', () => {
-		expect(trim(' 你好--aa ', '-a')).toBe('你好');
-		expect(trim(' 中文全角空格　　 ', '　')).toBe('中文全角空格');
-		expect(trim(' kkkkkkÂ\tâ', 'â')).toBe('kkkkkkÂ');
-		expect(trim(' a✓☒', '✓☒')).toBe('a');
+  it('trim custom charlist test', () => {
+    expect(trim(' 你好--aa ', '-a')).toBe('你好');
+    expect(trim(' 中文全角空格　　 ', '　')).toBe('中文全角空格');
+    expect(trim(' kkkkkkÂ\tâ', 'â')).toBe('kkkkkkÂ');
+    expect(trim(' a✓☒', '✓☒')).toBe('a');
 
-		expect(trim(' ab\u1d30', '\u1d30')).toBe('ab');
+    expect(trim(' ab\u1d30', '\u1d30')).toBe('ab');
 
-		expect(trim(' ab\u01D1', '\u004F\u030C')).toBe('ab');
-	});
+    expect(trim(' ab\u01D1', '\u004F\u030C')).toBe('ab');
+  });
 
-	it('trim no plus test', () => {
-		expect(trim(' 你好 --aa', '-a', trim.Replace)).toBe(' 你好 ');
-		expect(trim('   bbb b b   ', 'b', trim.Replace)).toBe('   bbb b b   ');
-		expect(trim("  kkkkkkÂ\tâ", 'â', trim.Replace)).toBe('  kkkkkkÂ\t');
-		expect(trim(' ab\u1d30', '\u1d30', trim.Replace)).toBe(' ab');
+  it('trim no plus test', () => {
+    expect(trim(' 你好 --aa', '-a', trim.Replace)).toBe(' 你好 ');
+    expect(trim('   bbb b b   ', 'b', trim.Replace)).toBe('   bbb b b   ');
+    expect(trim('  kkkkkkÂ\tâ', 'â', trim.Replace)).toBe('  kkkkkkÂ\t');
+    expect(trim(' ab\u1d30', '\u1d30', trim.Replace)).toBe(' ab');
 
-		expect(trim('\u004F\u030C\u004F\u030C ab\n\u01D1', '\u004F\u030C', trim.Replace)).toBe(' ab\n');
-	});
+    expect(trim('\u004F\u030C\u004F\u030C ab\n\u01D1', '\u004F\u030C', trim.Replace)).toBe(' ab\n');
+  });
 
-	it('ltrim', () => {
-		expect(ltrim(' aa--你好--aa ', '-a')).toBe('你好--aa ');
-		expect(ltrim('　　  \t中文全角空格　　 ', '　')).toBe('中文全角空格　　 ');
-		expect(ltrim("00--bbb b b\t\n\r", '0-', trim.Replace)).toBe('bbb b b\t\n\r');
-	});
+  it('ltrim', () => {
+    expect(ltrim(' aa--你好--aa ', '-a')).toBe('你好--aa ');
+    expect(ltrim('　　  \t中文全角空格　　 ', '　')).toBe('中文全角空格　　 ');
+    expect(ltrim('00--bbb b b\t\n\r', '0-', trim.Replace)).toBe('bbb b b\t\n\r');
+  });
 
-	it('trim.right test', () => {
-		expect(rtrim(' aa--你好--aa ', '-a')).toBe(' aa--你好');
-		expect(rtrim('　　  \t中文全角空格　　 ', '　')).toBe('　　  \t中文全角空格');
-		expect(rtrim("00--bbb b b00--00\t\n\r", '0-')).toBe('00--bbb b b');
-		expect(rtrim("/a/b/c///\\\\", '/\\')).toBe('/a/b/c');
-	});
-
-	it('toSafeString test', () => {
-		expect(toSafeString('\u004F\u030C\u004F\u030C ab\n\u01D1')).toBe('\u01D1\u01D1 ab\n\u01D1');
-		expect(toSafeString(['a', 'b', 'c', 'd'])).toBe('abcd');
-		expect(toSafeString(['a', 'b', 'c', 'd'], '/')).toBe('a/b/c/d');
-		expect(toSafeString(['a', 'b', ['c1', 'c2'], ['d1', 'd2']], '/')).toBe('a/b/c1/c2/d1/d2');
-		expect(toSafeString(new String('okokok'))).toBe('okokok');
-		expect(toSafeString(Symbol('hello world'))).toBe('Symbol(hello world)');
-		expect(toSafeString({})).toBe('[object Object]');
-	});
-
-	it('isString', () => {
-		expect(isString('aa')).toBe(true);
-		expect(isString(null)).toBe(false);
-		expect(isString(new String('okokok'))).toBe(true);
-		expect(isString({ok: 'ok'})).toBe(false);
-		expect(isString(['a', 'b', 'c', 'd'])).toBe(false);
-		expect(isString(Symbol('okokok'))).toBe(false);
-	});
-
-	it('isEmptyString', () => {
-		expect(isEmptyString('aa')).toBe(false);
-		expect(isEmptyString(null)).toBe(true);
-		expect(isEmptyString(undefined)).toBe(true);
-		expect(isEmptyString('')).toBe(true);
-		expect(isEmptyString('  ')).toBe(false);
-		expect(isEmptyString('0')).toBe(false);
-		expect(isEmptyString([])).toBe(false);
-		expect(isEmptyString({'ok': 'ok'})).toBe(false);
-	});
-
-	it('isEmptyStringOrWhitespace', () => {
-		expect(isEmptyStringOrWhitespace('aa')).toBe(false);
-		expect(isEmptyStringOrWhitespace(null)).toBe(true);
-		expect(isEmptyStringOrWhitespace(undefined)).toBe(true);
-		expect(isEmptyStringOrWhitespace('')).toBe(true);
-		expect(isEmptyStringOrWhitespace('  ')).toBe(true);
-		expect(isEmptyStringOrWhitespace('\t')).toBe(true);
-		expect(isEmptyStringOrWhitespace('\n')).toBe(true);
-		expect(isEmptyStringOrWhitespace([])).toBe(false);
-		expect(isEmptyStringOrWhitespace({'ok': 'ok'})).toBe(false);
-	});
+  it('trim.right test', () => {
+    expect(rtrim(' aa--你好--aa ', '-a')).toBe(' aa--你好');
+    expect(rtrim('　　  \t中文全角空格　　 ', '　')).toBe('　　  \t中文全角空格');
+    expect(rtrim('00--bbb b b00--00\t\n\r', '0-')).toBe('00--bbb b b');
+    expect(rtrim('/a/b/c///\\\\', '/\\')).toBe('/a/b/c');
+  });
 });
